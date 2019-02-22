3f8d6e2842dcb2c13b72dc600e72b48363eafa1b
kimgithub1 <36606642+kimgithub1@users.noreply.github.com>
2019-02-22 13:13:53 +0100

    Create settings.php

---
+++ resources/settings.php
@@ -0,0 +1,9 @@
+<?php
+return [
+        'AuthorizationBearer' => '',
+        'payeeId' => '',
+        'baseuri' => 'https://api.externalintegration.payex.com', // externaltest
+        //'baseuri' => 'https://api.payex.com', // production
+        'logging' => false
+    ];
+?>
