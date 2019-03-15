a92caabf1589337ed7ef2291701478bc77e706f9
kimgithub1 <36606642+kimgithub1@users.noreply.github.com>
2019-03-15 13:31:40 +0100

    Add files via upload

---
+++ resources/Curl.php
@@ -0,0 +1,112 @@
+<?php
+
+class Curl
+{
+    /**
+    * Logging method.
+    *
+    * @param string $log content to log.
+    * @param string $type LogLevel type.
+    *
+    * @return void
+    */
+    public function logger($log, $type)
+    {
+        $newlog = PHP_EOL . date("Y-m-d h:i:s") . PHP_EOL;
+        $newlog .= "LogLevel:" . $type . PHP_EOL;
+        $newlog .= $log;
+        $newlog .= "-------------------------";
+        $folder = 'logs';
+        if (!is_dir($folder)) {
+            mkdir($folder, 0777);
+        }
+        try {
+            file_put_contents($folder . '/log_' . date("Ymd") . '.txt', $newlog, FILE_APPEND);
+        } catch (Exception $e) {
+            //echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+    }
+
+    /**
+     * Curl Request.
+     *
+     * @param string $authorizationBearer
+     * @param string $httpMethod
+     * @param string $uri
+     * @param string $payload
+     *
+     * @return array
+     * response[0] = JSON Response data.
+     * response[1] = HTTP Code.
+     */
+    public function curlRequest($authorizationBearer, $httpMethod, $uri, $payload)
+    {
+        $CurlClass = new Curl();
+        $settingsData = include 'settings.php';
+
+        try {
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $uri);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            curl_setopt($curl, CURLOPT_SSLVERSION, 6); // TLS 1,2
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $httpMethod);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                "Authorization: Bearer " . $authorizationBearer,
+                "Content-Type: application/json; charset=utf-8",
+                "Accept: application/json",
+            ));
+
+            $response = curl_exec($curl);
+            $httpResponseCode = (curl_getinfo($curl, CURLINFO_HTTP_CODE));
+            $jsonResponse = json_decode($response);
+            $errno = curl_errno($curl);
+            $curlMessage = curl_error($curl);
+            curl_close($curl);
+        } catch (Exception $e) {
+            if ($settingsData['logging'] == true) {
+                $log = "";
+                $log .= "Caught exception: " . $e->getMessage() . PHP_EOL;
+                $CurlClass->logger($log, 'ERROR');
+            }
+        }
+
+        if ($errno == 0) {
+            if ($settingsData['logging'] == true) {
+                $message = '';
+                $log = "";
+                $log .= "Request method: " . $httpMethod . PHP_EOL;
+                $log .= "Request URI: " . $uri . PHP_EOL;
+                $log .= "Request: " . $payload . PHP_EOL;
+                $log .= "Response: " . $response . PHP_EOL;
+                if ($httpResponseCode == '200' || $httpResponseCode == '201') {
+                    $message = 'OK HTTP Request';
+                } elseif ($httpResponseCode == '400') {
+                    $message = 'Bad Request';
+                } elseif ($httpResponseCode == '401') {
+                    $message = 'OK HTTP Request, client Unauthorized';
+                } elseif ($httpResponseCode == '403') {
+                    $message = 'OK HTTP Request, client request forbidden';
+                } elseif ($httpResponseCode == '404') {
+                    $message = 'OK HTTP Request, but the resource you are looking for might have been removed, had its name changed, or is temporarily unavailable at URI ' . $uri;
+                }
+
+                $log .= "HTTP Code: " . $httpResponseCode . PHP_EOL;
+                $log .= 'Message: ' . $message . PHP_EOL;
+                $CurlClass->logger($log, 'INFO');
+            }
+            return array(
+                'response' => $jsonResponse,
+                'statusCode' => $httpResponseCode,
+            );
+        } else {
+            if ($settingsData['logging'] == true) {
+                $log = "";
+                $log .= "Curl error number: " . $errno . PHP_EOL;
+                $log .= "Curl message: " . $curlMessage . PHP_EOL;
+                $CurlClass->logger($log, 'ERROR');
+            }
+        }
+    }
+}
