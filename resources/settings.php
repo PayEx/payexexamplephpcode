b30c95f527f56a52b724d4b088bda9227203ba8e
kimgithub1 <36606642+kimgithub1@users.noreply.github.com>
2019-04-04 14:19:26 +0200

    Add files via upload

--- resources/settings.php
+++ resources/settings.php
@@ -3,7 +3,6 @@ return [
         'AuthorizationBearer' => '',
         'payeeId' => '',
         'baseuri' => 'https://api.externalintegration.payex.com', // externaltest
-        //'baseuri' => 'https://api.payex.com', // production
-        'logging' => false
+		//'baseuri' => 'https://api.payex.com', // production
+        'logging' => true
     ];
-?>
