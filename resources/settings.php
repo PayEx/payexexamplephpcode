bf745cf27487be34f9858a9d80a86eb80f4ab320
kimfjellbo <kim.fjellbo@payex.com>
2019-04-09 14:10:32 +0200

    remove files

--- resources/settings.php
+++ resources/settings.php
@@ -1,7 +1,7 @@
 <?php
 return [
-        'AuthorizationBearer' => '',
-        'payeeId' => '',
+        'AuthorizationBearer' => '654bb0e518c459926562fb553e7ddcd6089747b472cffa585e8b33d9de1e4011',
+        'payeeId' => 'b61b452d-577a-478e-8017-111621c064cc',
         'baseuri' => 'https://api.externalintegration.payex.com', // externaltest
 		//'baseuri' => 'https://api.payex.com', // production
         'logging' => true
