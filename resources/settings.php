d4af2d4e53e3181c5504722870545dcd3e2209cf
kimgithub1 <36606642+kimgithub1@users.noreply.github.com>
2019-07-19 21:00:50 +0200

    Update settings.php

--- resources/settings.php
+++ resources/settings.php
@@ -3,8 +3,8 @@
 namespace resources;
 
 return [
-        'AuthorizationBearer' => '654bb0e518c459926562fb553e7ddcd6089747b472cffa585e8b33d9de1e4011',
-        'payeeId' => 'b61b452d-577a-478e-8017-111621c064cc',
+        'AuthorizationBearer' => '',
+        'payeeId' => '',
         'baseuri' => 'https://api.externalintegration.payex.com', // externaltest
 		//'baseuri' => 'https://api.payex.com', // production
         'logging' => true
